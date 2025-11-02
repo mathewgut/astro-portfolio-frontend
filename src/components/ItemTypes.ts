b79a5271860287cf8a51b794677d49d1d5b0fa7c
Mathew Gutoskie <98121688+mathewgut@users.noreply.github.com>
2025-11-02 18:12:33 -0500

    change work item inspector to be two pages, restyle both pages, add workitem type

--- src/components/ItemTypes.ts
+++ src/components/ItemTypes.ts
@@ -19,6 +19,27 @@ export interface WorkArray {
     meta: {}
 }
 
+export interface WorkItem {
+    data: {
+        id: number;
+        documentId: string; 
+        title: string;
+        type: "website"|"web application"|"api"|"mobile"|"game"|"cli"|"chatbot"|"other";
+        role: "sole developer"|"sole designer"|"sole animator"|"sole engineer"|"frontend developer"|"backend developer"|"designer";
+        description: string;
+        is_active: boolean;
+        completed: Date;
+        media?:DocumentImage[];
+        link?:string;
+        technologies?:string;
+        articles:PostArray["data"];
+        createdAt: Date;
+        updatedAt: Date;
+        publishedAt: Date;
+    };
+    meta: {}
+}
+
 export interface PostArray {
     data: {
         id: number;
--- src/components/WorkContainer.tsx
+++ src/components/WorkContainer.tsx
@@ -1,23 +1,8 @@
-import { useEffect, useState, createContext, useContext, type ReactElement, type ReactNode } from "react";
-import MediaView from "./MediaView";
+import { useEffect, useState, type ReactNode } from "react";
 import type { ActiveItemType, PostArray, WorkArray } from "./ItemTypes";
 import { motion } from "motion/react";
 import { navigate } from "astro:transitions/client";
 import truncateText from "./utils";
-
-
-/* 
-
-images endpoint: res =  http://localhost:1337/api/works/{documentID}?populate=*
-
-// creates auto sizing for small, medium, large
-// 
-
-image_url = res.data.{size}.url
-
-*/
-
-
 
 export default function WorkContainer(){
     const [works, setWorks] = useState<WorkArray | null>(null);
@@ -27,7 +12,7 @@ export default function WorkContainer(){
     
     const [error, setError] = useState<any>(null);
     const [activeError, setActiveError] = useState<any>(null);
-    const [mediaActive, setMediaActive] = useState<boolean>(false)
+    const [mediaActive, setMediaActive] = useState<boolean>(false);
     
     const [activeItem, setActiveItem] = useState<ActiveItemType | null>(null);
 
@@ -77,160 +62,97 @@ export default function WorkContainer(){
     
     return(
         <>
-        { !loadingActive && mediaActive && activeItem?.media &&
-            <MediaView media={activeItem.media} 
-            active={mediaActive} setActive={setMediaActive} />
-        }
-        
-        
-        <main 
-        className="flex flex-col mt-10 gap-2 pb-10 w-full h-full font-[Terminal] ">
-            
-            <section className="flex flex-col sm:flex-row h-full gap-2">
-                <aside className="flex flex-col h-full min-h-2/5 sm:min-h-fit sm:w-1/2 sm:min-w-75 sm:max-w-120 border-2 rounded-xl px-4 overflow-y-scroll">
-                    <header className="sticky top-0 z-20 bg-white flex items-center justify-center border-b-2">
-                        <h2>
-                            Project select
-                        </h2>
-                    </header>
-                    <section className="flex min-h-fit gap-4 flex-col m-2">
-                        {
-                            loading &&
-                            <p className="flex justify-center duration-200 ease-in-out animate-pulse text-neutral-600">Syncing data...</p>
-                        }
-                        { 
-                            !loading && works?.data?.map((item,index) =>
-                            <>
-                                <ProjectItem key={item.id} item={item} onClickFn={fetchWorkItem} />
-                            </>
-                        )}
-                    </section>
-                </aside>
-                <aside className="flex flex-col overflow-y-auto sm:w-full rounded-xl px-4 justify-start items-center text-black">
-                    <section className="flex flex-col mt-2 justify-start items-center">
-                        {
-                            loadingActive &&
-                            <p className="duration-200 ease-in-out animate-pulse text-neutral-600">Decrypting...</p>
-                        }
-                        
-                        {
-                            !loadingActive && !activeItem &&
-                            <>
-                                <h3>Inspector</h3>
-                                <img className="h-5 w-5" alt="Magnifying glass icon" src="/search.svg" />
-                            </>
-                        }
-                        {
-                            !loadingActive && activeItem &&
-                            <section className="flex flex-col justify-start items-center px-6 md:px-12 h-full">
-                                <article className="flex flex-col gap-2 justify-start items-center h-fit">
-                                    <p>ID: {activeItem.id}</p>
-                                    <h3 className="text-xl font-semibold">{activeItem.title}</h3>
-                                    <div className="flex gap-2 mb-2 text-sm">
-                                        <Badge title={activeItem.type}>
-                                            <img src="/folder.svg" alt="Project type icon" className="h-5 w-5" style={{ filter: "invert(1)" }} />
-                                        </Badge>
-
-                                        <Badge title={activeItem.role}>
-                                            <img src="/user.svg" alt="Role icon" className="h-5 w-5" style={{ filter: "invert(1)" }} />
-                                        </Badge>
-                                    </div>
-                                    <p className="my-6">{activeItem.description}</p>
-                                    { activeItem.completed &&
-                                        <span className="flex flex-col w-full">
-                                            <p className="mb-1 font-semibold">
-                                                Completed
-                                            </p>
-                                            <p className="text-xs">
-                                                {new Date(activeItem.completed).toDateString()}
-                                            </p>
-                                        </span>
-
-                                    }
-                                    
-                                    
-                                    { activeItem.technologies &&
-                                        <span className="flex flex-col w-full">
-                                            <p className="mb-1 font-semibold">
-                                                Technologies
-                                            </p>
-                                            <p className="text-xs">
-                                                {activeItem.technologies}
-                                            </p>
-                                        </span>
-                                    }
-
-                                    { activeItem.link &&
-
-                                        <Link url={activeItem.link}/>
-                                        
-                                    }
-                                    <div className="flex gap-2">
-                                        { activeItem.media &&
-                                            <p onClick={() => setMediaActive(true)} className="text-black hover:underline hover:cursor-pointer">
-                                                View media    
-                                            </p>
-                                        }
-                                    
-                                    </div>
-                                </article>
-                                
-                                { activeItem.articles.length > 0 &&
-                                    <BlogPosts posts={activeItem.articles} />
-                                    
-                                }
-                            </section>
-
-                        }
-                    </section>
-                </aside>
-            </section>
-        </main>
+            <ProjectSelect loading={loading} works={works} fetchWorkItem={fetchWorkItem} />
         </>
 
+    )
+}
+
+function ProjectSelect({loading, works, fetchWorkItem}:{loading:boolean, works:WorkArray | null, fetchWorkItem:(id:string)=>void}){
+    const [timeoutOpacity, setTimeoutOpacity] = useState<number>(0);
+    const headerTextStyles = "text-xs flex gap-2 items-center";
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setTimeoutOpacity(1);
+        }, 500);
+        return () => clearTimeout(timeout);
+    }, []);
+    
+    return(
+        <main className="flex flex-col min-h-svh w-svh rounded-xl px-4">
+            <header className="mb-6">
+            <h1 className="mt-6 mb-2 px-4 py-2 bg-black w-fit text-white text-lg sm:text-3xl">
+                // PROJECT_DIRECTORY
+            </h1>
+            { loading &&
+                <p className={headerTextStyles}>Retrieving projects...</p>
+            }
+            { !loading &&
+                <div className={headerTextStyles}>
+                    {loading && "Loading projects..." }
+                    {!loading && "Success."} 
+                    <span className="ease-in-out duration-500 delay-200" style={{opacity: timeoutOpacity}}>
+                        {works?.data.length} projects found.
+                    </span>
+                </div>
+            }
+            
+            </header>
+            <section className="flex min-h-fit gap-4 flex-col m-2 pb-2">
+                { 
+                    works?.data?.map((item,index) =>
+                    <motion.div
+                    initial={{opacity:0, y:20}}
+                    animate={{opacity:1, y:0}}
+                    transition={{delay: index * 0.2, duration:0.3}}
+                    >
+                        <ProjectItem key={item.id} item={item} onClickFn={(id:string) => navigate("works/"+id)} />
+                    </motion.div>
+                )}
+            </section>
+        </main>
     )
 }
 
 function ProjectItem ({item, onClickFn}:{item:WorkArray["data"][number], onClickFn:(id:string)=>void}) {
     const colors = ["border-blue-900", "border-green-900", "border-slate-900", "border-red-900", "border-indigo-900", "border-rose-900", "border-teal-900", "border-violet-900"]
     const selectedColor = colors[Math.floor(Math.random()*colors.length)]
-    const badgeDelay = 0.5;
+    const badgeDelay = 0;
 
     return(
-        <motion.div
-        initial={{height:0}}
-        animate={{minHeight:"fit-content",height:"fit-content"}}
-        transition={{
-            duration:0.3,
-            ease:"easeInOut",
-            }}
-        whileInView="visible"
-        viewport={{ once: true }}
+        <div
         onClick={() => onClickFn(item.documentId)} 
-        className={`flex flex-col justify-end px-4 py-2 ${selectedColor} border-b-2 hover:border-b-4 h-30 w-full col-span-1 text-black hover:cursor-pointer ease-in-out duration-200 hover:scale-105 hover:pb-4`}>
-            <motion.h2 
-                initial={{opacity:0}}
-                animate={{opacity:1}}
-                transition={{
-                    delay:0.3,
-                    repeat:0,
-                    duration:0.3
-                }}
-                
+        className={`flex flex-col justify-end px-4 py-2 ${selectedColor} border-b-2 hover:border-b-4 w-full col-span-1 text-black hover:cursor-pointer ease-in-out duration-200 hover:scale-105 hover:pb-4`}>
+            <h2
             className="text-xl pb-2">
-                {item.title}
+               [ {item.title} ]
 
-            </motion.h2>
-            <div className="flex gap-2 capitalize text-sm text-white">
-                <Badge delay={badgeDelay} title={item.type}>
-                    <img src="/folder.svg" alt="Project icon" className="h-5 w-5" style={{ filter: "invert(1)" }} />
-                </Badge>
+            </h2>
+            <p className="text-xs text-neutral-700">
+                {truncateText(item.description, 125) }
+            </p>
+
+
+            <div className="flex flex-col sm:flex-row sm:gap-6 mt-6 mb-2 sm:mb-0 capitalize text-sm items-center hover:cursor-pointer">
+                <p className="text-black mb-2 sm:mb-0">// TAGS</p>
+                <div className="flex flex-col sm:flex-row gap-2">
+                    <Badge delay={badgeDelay} title={item.type}>
+                        <img src="/folder.svg" alt="Project icon" className="h-4 w-4" />
+                    </Badge>
+                    
+                    <Badge delay={badgeDelay} title={item.role}>
+                        <img src="/user.svg" alt="Role icon" className="h-4 w-4" />
+                    </Badge>
+                </div>
                 
-                <Badge delay={badgeDelay} title={item.role}>
-                    <img src="/user.svg" alt="Role icon" className="h-5 w-5" style={{ filter: "invert(1)" }} />
-                </Badge>
             </div>
-        </motion.div>
+            <div className="flex flex-col sm:flex-row items-center sm:gap-6 text-sm mt-2">
+                    <p className="w-fit whitespace-nowrap mb-2 sm:mb-0">// STACK</p>
+                    <p className="text-xs">{item.technologies}</p>
+                </div>
+            
+        </div>
     )
 }
 
@@ -245,7 +167,7 @@ export function Badge({title, children, delay=0}:{title:string, children:ReactNo
             delay:delay,
             duration:delay
         }}
-        className="flex gap-1 capitalize text-sm justify-center items-center text-white bg-black py-1 px-3 rounded-lg hover:cursor-default">
+        className="flex gap-1 capitalize text-xs justify-center items-center text-black border-2 border-black py-1 px-2.5 rounded-lg hover:cursor-default">
             {children}
             <p className="text-xs">
                 {title}
@@ -257,8 +179,8 @@ export function Badge({title, children, delay=0}:{title:string, children:ReactNo
 function BlogPosts({posts}:{posts:PostArray["data"]}){
     
     return(
-        <section className="flex flex-col mt-4 w-3/4">
-            <h3 className="font-semibold">
+        <section className="flex flex-col mt-4 md:w-3/4">
+            <h3 className="flex items-center justify-center md:justify-start font-semibold">
                 Associated Posts
             </h3>
             <section className="flex flex-col text-black overflow-y-scroll p-4 h-50">
@@ -280,9 +202,17 @@ function BlogPosts({posts}:{posts:PostArray["data"]}){
 
 function Link({url}:{url:string}){
     return(
-        <span className="group flex gap-2 items-center w-full h-fit underline hover:cursor-pointer px-1 py-2 hover:bg-black hover:text-white text-sm">
-            <img className="h-5 w-5 group-hover:invert-100" src="/link.svg"></img>
-            <a referrerPolicy="no-referrer" href={url}>{url}</a>
-        </span>
+        <a href={url} referrerPolicy="no-referrer" className="group flex gap-2 items-center justify-center w-full h-fit underline hover:cursor-pointer px-1 py-2 hover:bg-black hover:text-white text-sm">
+            <img className="h-5 w-5 group-hover:invert-100" alt="link icon" src="/link.svg"></img>
+            <p>{url}</p>
+        </a>
+    )
+}
+
+function ProjectStatus({isActive}:{isActive:boolean}){
+    return(
+        <div className={`flex justify-center items-center gap-2 px-2 py-1 rounded-md text-xs font-semibold ${isActive ? "bg-green-200 text-green-800" : "bg-red-200 text-red-800"}`}>
+            {isActive ? "ACTIVE" : "INACTIVE"}
+        </div>
     )
 }
